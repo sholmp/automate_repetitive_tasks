41b09bc45ca2243c5e66ebaaa918f29f9449ec26
soren.holmptersen <soren.holmpetersen@cobham.com>
2021-08-13 16:47:48 +0200

    refactoring

--- tempo.py
+++ tempo.py
@@ -14,30 +14,30 @@ logTimeHeight = 310
 
 logTimePoints = [Point(175, logTimeHeight), Point(395, logTimeHeight), Point(615, logTimeHeight), Point(835, logTimeHeight), Point(1055, logTimeHeight)]
 
+
+
 def logTime():
-    os.system("xdotool click 1")
-    time.sleep(1)
-    os.system("xdotool type 'tempo'")
-    time.sleep(1)
-    os.system("xdotool key Return")
-    time.sleep(1)
-    os.system("xdotool key Tab")
-    time.sleep(1)
-    os.system("xdotool key Tab")
-    os.system("xdotool key Tab")
-    os.system("xdotool key Tab")
-    time.sleep(1)
-    os.system("xdotool type 7,5")    
-    os.system("xdotool key Return")
-    time.sleep(1)
-    os.system("xdotool key Tab")
-    os.system("xdotool key Tab")
-    os.system("xdotool key Return")
+    xDoToolCommands = ["xdotool click 1",
+        "xdotool type 'tempo'",
+        "xdotool key Return",
+        "xdotool key Tab", 
+        "xdotool key Tab",
+        "xdotool key Tab",
+        "xdotool key Tab",
+        "xdotool type 7,5",
+        "xdotool key Return",
+        "xdotool key Tab",
+        "xdotool key Tab",
+        "xdotool key Return"]
+    for c in xDoToolCommands:
+        os.system(c)
+        time.sleep(0.2)
 
 
 if __name__ == "__main__":
     for p in logTimePoints:
         os.system("xdotool mousemove {} {}".format(p.x,p.y))
-        time.sleep(1)
         logTime()
+        time.sleep(1)
+
 
